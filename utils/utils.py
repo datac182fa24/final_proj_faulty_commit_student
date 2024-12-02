efc9286fb9e8d54a91b495c52c187e35ba524be8
Eric Kim <erickim555@gmail.com>
2024-12-01 23:39:05 -0800

    Actually fix add-entry

--- utils/utils.py
+++ utils/utils.py
@@ -53,7 +53,7 @@ def add_entry_to_auto_grader_data(auto_grader_data: Dict[str, Any], keys: List[s
     """
     cur_dict = auto_grader_data
     for key in keys[:-1]:
-        if key not in auto_grader_data:
+        if key not in cur_dict:
             cur_dict[key] = {}
         cur_dict = cur_dict[key]
     cur_dict[keys[-1]] = value
